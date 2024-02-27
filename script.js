2cc7aa61b7ff6f6a83d2e205601fade52e0ee3c3
Opilane TTHK <opilane@tthk.ee>
2024-02-27 10:26:36 +0200

    lisasin erdid

--- script.js
+++ script.js
@@ -1,30 +1,17 @@
-function changeStyle() {
 
-    const button = document.getElementById("stylebutton");
-
-    if (button.classList.contains("button-blue")) {
-
-        button.classList.remove("button-blue");
-
-        button.classList.add("button-red");
-
-    } else {
-
-        button.classList.remove("button-red");
-
-        button.classList.add("button-blue");
-
-    }
-
-}
 function changeStyle1() {
     const body = document.getElementById("stylebody");
 
     if (body.classList.contains("body1")) {
         body.classList.remove("body1");
         body.classList.add("body2");
-    } else {
+    } 
+    else if (body.classList.contains("body2")){
         body.classList.remove("body2");
+        body.classList.add("body3");
+    }
+    else {
+        body.classList.remove("body3");
         body.classList.add("body1");
         body.style.animationPlayState = "paused";
         body.offsetWidth = body.offsetWidth; 
